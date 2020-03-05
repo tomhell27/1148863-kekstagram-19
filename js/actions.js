9dc4985665720ef2c68cd9c0bcb9d07fd1be9ba6
tomhell27 <53575983+tomhell27@users.noreply.github.com>
2020-03-06 01:30:58 +0300

    21. Личный проект: фото на память (необязательное задание)

--- js/actions.js
+++ js/actions.js
@@ -2,6 +2,7 @@
 'use strict';
 
 (function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
   var modalOpen = document.querySelector('#upload-file');
   var modalClose = document.querySelector('#upload-cancel');
   var modalPicture = document.querySelector('.img-upload__overlay');
@@ -9,8 +10,28 @@
   var modalComments = document.querySelector('.text__description');
   var imgUpload = document.querySelector('.img-upload__preview');
   var imgEffect = imgUpload.querySelector('img');
+  var previewImg = document.querySelectorAll('.effects__preview');
+  var createPreview = function (img) {
+    previewImg.forEach(function (e) {
+      e.style.backgroundImage = 'url(' + img + ')';
+    });
+  };
 
-
+  modalOpen.addEventListener('change', function () {
+    var file = modalOpen.files[0];
+    var fileName = file.name.toLowerCase();
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+    if (matches) {
+      var reader = new FileReader();
+      reader.addEventListener('load', function () {
+        imgEffect.src = reader.result;
+        createPreview(reader.result);
+      });
+      reader.readAsDataURL(file);
+    }
+  });
   var openModal = function () {
     modalPicture.classList.remove('hidden');
     document.addEventListener('keydown', window.utils.onModalEscPress);
